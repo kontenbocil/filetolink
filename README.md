1558987f0fc2b38e4b45aaaf43791d7a2017311c
derylprojects <87072063+derylprojects@users.noreply.github.com>
2021-07-07 19:40:12 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# filetolink
-File to link
+[![Issues](https://img.shields.io/github/issues/deryl/filetolink?style=for-the-badge&color=green)](https://github.com/deryl/filetolink/issues)
+[![Forks](https://img.shields.io/github/forks/deryl/filetolink?style=for-the-badge&color=green)](https://github.com/deryl/filetolink/fork)
+[![Stars](https://img.shields.io/github/stars/deryl/filetolink?style=for-the-badge&color=green)](https://github.com/deryl/filetolink)
+[![Size](https://img.shields.io/github/repo-size/deryl/filetolink?style=for-the-badge&color=green)](https://github.com/deryl/filetolink)
+[![Contributors](https://img.shields.io/github/contributors/deryl/filetolink?style=for-the-badge&color=green)](https://github.com/derylprojectss/filetolink)
+
+
+# UPLOAD
+
+## ⬆️DEPLOY KE SERVER HEROKU
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://rekonise.com/subs-dulu-mtu0s)
+
+
+## APA ITU HEROKU
+HEROKU ADALAH SISTEM PEMBUAT APLIKASI WEB AGAR BERJALAN
+24 JAM
+## KELEMAHAN
+```
+TERBATAS SAMPAI 5 APP SAJA, JIKA SUDAH 5 APP KITA HARUS MENGHAPUS SALAH SATU APP TERLEBIH DAHULU
+```
---
+++ bot.py
@@ -0,0 +1,41 @@
+import telebot
+import time
+import pyshorteners
+import os
+
+bot = telebot.TeleBot(token=os.getenv('TG_BOT_TOKEN'))
+
+def short(url):
+    return pyshorteners.Shortener().tinyurl.short(url)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Hai! Kirim file untuk di pendekkan \n Jangan lupa join @derylproject untuk mendapatkan uang')
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, 'Kirim file dan saya akan mengrimkanmu linknya \n Join @derylproject')    
+
+@bot.message_handler(content_types=['photo', 'video', 'audio', 'document'])
+def file_sent(message):
+    try:
+        bot.send_message(message.chat.id, short(bot.get_file_url(message.document.file_id)))
+    except AttributeError:
+        try:
+            bot.send_message(message.chat.id, short(bot.get_file_url(message.photo[0].file_id)))
+        except AttributeError:
+            try:
+                bot.send_message(message.chat.id, short(bot.get_file_url(message.audio.file_id)))
+            except AttributeError:
+                try:
+                    bot.send_message(message.chat.id, short(bot.get_file_url(message.video.file_id)))
+                except AttributeError:
+                    pass
+
+
+while True:
+    try:
+        bot.polling()
+    except Exception:
+        time.sleep(15)
+
